0d9aeeff5d381d234697918909de082212d59181
spyda247 <babajideibiayo@yahoo.com>
2020-07-03 19:31:41 +0100

    Prepared the child component to emit an event using the Output Decorator

--- src/app/rental-list/rental-list.component.ts
+++ src/app/rental-list/rental-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'my-rental-list',
@@ -7,6 +7,11 @@ import { Component, Input } from '@angular/core';
 })
 export class RentalListComponent  {
   @Input() rentalList;
+  @Output() clearMovies = new EventEmitter().emit();
+
+  onRentalListButtonClicked() {
+    // Inform parent
+  }
 }
 
 /*
